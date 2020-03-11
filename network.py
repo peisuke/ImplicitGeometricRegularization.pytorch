9f81cb5f19315c2e949db29f80ecc76c44cb3d17
peisuke <peisuke.com@gmail.com>
2020-03-11 04:55:31 +0000

    fix

--- network.py
+++ network.py
@@ -5,24 +5,6 @@ import torch.nn.functional as F
 class Network(nn.Module):
     def __init__(self, input_dim):
         super(Network, self).__init__()
-        self.l1 = nn.Linear(input_dim, 128)
-        self.l2 = nn.Linear(128, 128 - input_dim)
-        self.l3 = nn.Linear(128, 128)
-        self.l4 = nn.Linear(128, 128)
-        self.l5 = nn.Linear(128, 1)
-
-    def forward(self, x):
-        h = F.softplus(self.l1(x), beta=100)
-        h = F.softplus(self.l2(h), beta=100)
-        h = torch.cat((h, x), axis=1)
-        h = F.softplus(self.l3(h), beta=100)
-        h = F.softplus(self.l4(h), beta=100)
-        h = self.l5(h)
-        return h
-
-class NetworkLarge(nn.Module):
-    def __init__(self, input_dim):
-        super(NetworkLarge, self).__init__()
         self.l1 = nn.Linear(input_dim, 512)
         self.l2 = nn.Linear(512, 512)
         self.l3 = nn.Linear(512, 512)
